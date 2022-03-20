0c7d63c79156ee80e25bdf96ec638c61bbaf5cda
juliano <julianoaugusto22@gmail.com>
2022-03-20 18:41:14 -0300

    Refatorando alertas para metodo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,47 +1,58 @@
 module ApplicationHelper
-    def login_helper style = ''
-        if current_user.is_a?(GuestUser)
-            (link_to "Login", new_user_session_path, class: style) +
-            " ".html_safe +
-            (link_to "Register", new_user_registration_path, class: style)
-        else
-            link_to("Logout", destroy_user_session_path, method: :delete, class: style)
-        end
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Login", new_user_session_path, class: style) +
+      " ".html_safe +
+      (link_to "Register", new_user_registration_path, class: style)
+    else
+      link_to("Logout", destroy_user_session_path, method: :delete, class: style)
+    end
+  end
+
+  def source_helper(layout)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright('Juliano Marques', 'All rights reserved')
+  end
+
+  def nav_items
+    [
+      { url: root_path, title: 'Home' } ,
+      { url: about_path, title: 'About' } ,
+      { url: contact_path, title: 'Contact' } ,
+      { url: blogs_path, title: 'Blogs' } ,
+      { url: portfolios_path, title: 'Portfolio' } ,
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
-    def source_helper(layout)
-        if session[:source]
-            greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout} layout"
-            content_tag(:p, greeting, class: "source-greeting")
-        end
-    end
+    nav_links.html_safe
+  end
 
-    def copyright_generator
-        DevcampViewTool::Renderer.copyright('Juliano Marques', 'All rights reserved')
-    end
+  def active? path
+    "active" if current_page? path
+  end
 
-    def nav_items
-        [
-            { url: root_path, title: 'Home' } ,
-            { url: about_path, title: 'About' } ,
-            { url: contact_path, title: 'Contact' } ,
-            { url: blogs_path, title: 'Blogs' } ,
-            { url: portfolios_path, title: 'Portfolio' } ,
-        ]        
-    end
-    
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
 
-    def nav_helper style, tag_type
-        nav_links = ''
-
-        nav_items.each do |item|
-            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
-        end
-        
-        nav_links.html_safe
+    if alert
+      alert_generator(alert)
     end
+  end
 
-    def active? path
-        "active" if current_page? path
-    end
+  def alert_generator(msg)
+    js add_gritter(msg, :title => "Juliano Portfolio", :stick => false, :class_name => "custom-gritter")
+  end
 end
